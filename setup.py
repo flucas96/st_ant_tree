5dc17e9f20de847600f1d23b550a513561061fc8
fl <fl@msr.de>
2025-01-26 13:15:59 +0100

    clean up

--- setup.py
+++ setup.py
@@ -13,9 +13,14 @@ setuptools.setup(
     long_description=long_desc,
     long_description_content_type='text/markdown',
     url="https://github.com/flucas96/st_ant_tree",
+    license="MIT",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    classifiers=[],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
     python_requires=">=3.6",
     install_requires=[
         # By definition, a Custom Component depends on Streamlit.
