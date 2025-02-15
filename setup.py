4f4bafb531f49402969ddda5710e25f2164c7b94
fl <fl@msr.de>
2025-02-15 01:18:19 +0100

    version up

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setuptools.setup(
     name="st_ant_tree",
-    version="0.0.13",
+    version="0.0.14",
     author="",
     author_email="",
     description="Streamlit Component for ANT Tree",
